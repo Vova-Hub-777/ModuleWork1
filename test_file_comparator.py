abe2cd4004db013219959a86f87a006d6e8baa89
Vova-Hub-777 <vovavitkovskiy47@gmail.com>
2023-03-29 14:42:10 +0300

    Додано тестову функцію для перевірки правильності читання файлу.

--- test_file_comparator.py
+++ test_file_comparator.py
@@ -11,3 +11,11 @@ def sample_files(tmpdir):
     file2.write("Line 2\nLine 3\nLine 4\n")
 
     return str(file1), str(file2)
+
+def test_read_file(sample_files):
+    file1, file2 = sample_files
+    file1_content = read_file(file1)
+    file2_content = read_file(file2)
+
+    assert file1_content == ["Line 1\n", "Line 2\n", "Line 3\n"]
+    assert file2_content == ["Line 2\n", "Line 3\n", "Line 4\n"]
