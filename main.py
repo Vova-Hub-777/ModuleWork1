5c022c1b6212ab205aecedb198208e5ae54aa18e
Vova-Hub-777 <vovavitkovskiy47@gmail.com>
2023-03-29 14:28:18 +0300

    Реалізація основної функції.

--- main.py
+++ main.py
@@ -9,3 +9,28 @@ def read_file(file_name):
 def write_file(file_name, content):
     with open(file_name, 'w') as file:
         file.writelines(content)
+
+def main():
+    file1_name = input("Enter the name of the first file (with .txt extension): ")
+    file2_name = input("Enter the name of the second file (with .txt extension): ")
+
+    file1_content = read_file(file1_name)
+    file2_content = read_file(file2_name)
+
+    same_content = []
+    diff_content = []
+
+    for line in file1_content:
+        if line in file2_content:
+            same_content.append(line)
+        else:
+            diff_content.append(line)
+
+    for line in file2_content:
+        if line not in file1_content:
+            diff_content.append(line)
+
+    write_file("same.txt", same_content)
+    write_file("diff.txt", diff_content)
+
+    print("Comparison completed. Results saved in 'same.txt' and 'diff.txt'.")
